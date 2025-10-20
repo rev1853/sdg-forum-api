b0fee5281602290ede5dea0cbc9b2596c032f12e
rev1853 <rghofur053@gmail.com>
2025-10-20 11:17:42 +0700

    done

--- src/services/categoryService.js
+++ src/services/categoryService.js
@@ -1,20 +1,21 @@
 const prisma = require('../prisma');
 
 const listCategories = async () => {
-  const categories = await prisma.category.findMany({
-    orderBy: { sdg_number: 'asc' },
-    select: {
-      id: true,
-      name: true,
-      sdg_number: true,
-      created_at: true
-    }
-  });
+    const categories = await prisma.category.findMany({
+        orderBy: { sdg_number: 'asc' },
+        select: {
+            id: true,
+            name: true,
+            sdg_number: true,
+            created_at: true
+        }
+    });
 
-  return categories;
+    return categories;
 };
 
+
 module.exports = {
-  listCategories
+    listCategories
 };
 
