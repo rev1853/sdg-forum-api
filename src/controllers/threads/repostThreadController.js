683a00daeddc6dcc1d2bf77b5a20fe24f18f97e3
rev1853 <rghofur053@gmail.com>
2025-10-20 11:20:13 +0700

    done

--- src/controllers/threads/repostThreadController.js
+++ src/controllers/threads/repostThreadController.js
@@ -1,8 +1,9 @@
 const asyncHandler = require('../../utils/asyncHandler');
 const { repostThread } = require('../../services/threadService');
 
+
 module.exports = asyncHandler(async (req, res) => {
-  const interaction = await repostThread(req.params.threadId, req.user.id);
-  res.status(201).json({ interaction });
+    const interaction = await repostThread(req.params.threadId, req.user.id);
+    res.status(201).json({ interaction });
 });
 
